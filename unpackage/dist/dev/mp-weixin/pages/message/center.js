bc257ad9e7a8f6af5484baf130f12efab4ceb58f
zep912 <xuzepeng912@163.com>
2019-12-09 21:03:15 +0800

    add

---
+++ unpackage/dist/dev/mp-weixin/pages/message/center.js
@@ -0,0 +1,32 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/message/center"],{
+
+/***/ 376:
+/*!*****************************************************************************!*\
+  !*** /Users/xuyi/dev/sh/m-demo/main.js?{"page":"pages%2Fmessage%2Fcenter"} ***!
+  \*****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);__webpack_require__(/*! @dcloudio/uni-stat */ 5);
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _center = _interopRequireDefault(__webpack_require__(/*! ./pages/message/center.vue */ 377));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_center.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 377:
+/*!**********************************************************!*\
+  !*** /Users/xuyi/dev/sh/m-demo/pages/message/center.vue ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open '/Users/xuyi/dev/sh/m-demo/pages/message/center.vue'");
+
+/***/ })
+
+},[[376,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/message/center.js.map
---
+++ unpackage/dist/dev/mp-weixin/pages/message/orderWarn.js
@@ -0,0 +1,32 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["pages/message/orderWarn"],{
+
+/***/ 392:
+/*!********************************************************************************!*\
+  !*** /Users/xuyi/dev/sh/m-demo/main.js?{"page":"pages%2Fmessage%2ForderWarn"} ***!
+  \********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);__webpack_require__(/*! @dcloudio/uni-stat */ 5);
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _orderWarn = _interopRequireDefault(__webpack_require__(/*! ./pages/message/orderWarn.vue */ 393));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_orderWarn.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 393:
+/*!*************************************************************!*\
+  !*** /Users/xuyi/dev/sh/m-demo/pages/message/orderWarn.vue ***!
+  \*************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open '/Users/xuyi/dev/sh/m-demo/pages/message/orderWarn.vue'");
+
+/***/ })
+
+},[[392,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/message/orderWarn.js.map
